c6801d339b75302978ed16c1b710e3a1bcdfc8f7
Developer From Jokela <developerfromjokela@gmail.com>
2020-04-13 15:07:20 +0300

    Fixed bugs

--- config/config.php
+++ config/config.php
@@ -8,4 +8,5 @@
 // TODO Add your FCM Server key. You can find it in Firebase control panel (Firebase Console -> Project Settings -> CLOUD MESSAGING -> Server key)
 define("FCM_SERVER_KEY", "YOUR_KEY");
 
+// TODO Remember to disable debug mode after everything is confirmed to work!
 define("DEBUG", true);
